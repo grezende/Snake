fce9a541c983a0a77feed2f360ef19813c4705e9
grezende <grezende91@gmail.com>
2017-12-12 16:28:37 -0200

    Added eye to snake head

--- GazeusSnake/Models/SnakeHead.swift
+++ GazeusSnake/Models/SnakeHead.swift
@@ -10,5 +10,20 @@ import SpriteKit
 
 class SnakeHead: SKSpriteNode{
     
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+        
+        let eye = SKSpriteNode(color: SKColor.black,
+                               size: CGSize(width: self.size.width / 4,
+                                            height: self.size.height / 4))
+        
+        eye.position = CGPoint(x: self.size.width / 4 * 3 - self.size.width / 2,
+                               y: self.size.height / 4 * 3 - self.size.height / 2)
+        
+        self.addChild(eye)
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
