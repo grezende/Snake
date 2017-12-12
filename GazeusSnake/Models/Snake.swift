a61cdfc55201f4c92643be98fd6a5aaa316e0d52
grezende <grezende91@gmail.com>
2017-12-12 16:20:52 -0200

    Added initial snake

--- GazeusSnake/Models/Snake.swift
+++ GazeusSnake/Models/Snake.swift
@@ -11,16 +11,33 @@ import SpriteKit
 class Snake{
     
     var head: SnakeHead?
-    var segments: SnakeSegment?
+    var segments: [SnakeSegment] = []
     let initialNumberOfSegments = 3
+    var segmentSize: CGSize?
     
     func setSnake(scene: GameScene){
         
+        self.segmentSize = CGSize(width: scene.size.width / 40,
+                                  height: scene.size.height / 20
+        )
+        
         self.head = SnakeHead(color: SKColor.green,
-                              size: CGSize(width: scene.size.width / 40,
-                                           height: scene.size.height / 20))
+                              size: self.segmentSize!)
         
         self.head?.position = CGPoint(x: scene.size.width / 2, y: scene.size.height / 2)
+        
+        for i in 1...initialNumberOfSegments{
+            
+            let newSegment = SnakeSegment(color: SKColor.green, size: self.segmentSize!)
+            newSegment.position = CGPoint(
+                x: (self.head?.position.x)! - CGFloat(i) * (segmentSize?.width)!,
+                y: (self.head?.position.y)!)
+            self.segments.append(newSegment)
+        }
+        
+        for s in self.segments{
+            scene.addChild(s)
+        }
         
         scene.addChild(head!)
     }
--- GazeusSnake/Models/SnakeSegment.swift
+++ GazeusSnake/Models/SnakeSegment.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2017 Gabriel Rezende. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class SnakeSegment: SKSpriteNode{
+    
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
